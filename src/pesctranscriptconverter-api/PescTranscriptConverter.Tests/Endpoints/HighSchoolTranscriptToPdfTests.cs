32f51b6366ff6375c86379f042f9d73e1889212e
maldworth <m@maldworth.com>
2025-02-07 11:55:40 -0500

    Revert TUnit to XUnit
    - the new TUnit and even the new XUnit have issues with the new MTP (Microsoft.Testing.Platform) and tests hang when running. Since it's a "preview" feature,
    I think we have to wait for this to become fully released

--- src/pesctranscriptconverter-api/PescTranscriptConverter.Tests/Endpoints/HighSchoolTranscriptToPdfTests.cs
+++ src/pesctranscriptconverter-api/PescTranscriptConverter.Tests/Endpoints/HighSchoolTranscriptToPdfTests.cs
@@ -1,43 +1,42 @@
-﻿namespace PescTranscriptConverter.Tests.Endpoints;
+﻿using Microsoft.Extensions.DependencyInjection;
 
-[ParallelLimiter<DafParallelLimit>]
-[ClassDataSource<Daf>(Shared = SharedType.None)]
+namespace PescTranscriptConverter.Tests.Endpoints;
+
+[CollectionDefinition(nameof(HighSchoolTranscriptToPdfTests))]
+public class HighSchoolTranscriptToPdfTestsCollection: ICollectionFixture<Fixture>;
+
+[Collection(nameof(HighSchoolTranscriptToPdfTests))]
 public class HighSchoolTranscriptToPdfTests
 {
-    private readonly Daf _daf;
+    private PescTranscriptConverterClient? _apiClient;
 
-    public HighSchoolTranscriptToPdfTests(Daf daf)
+    public HighSchoolTranscriptToPdfTests(Fixture fixture)
     {
-        _daf = daf;
+        _apiClient = fixture.GetApiClient();
     }
 
-    [Test]
-    [Arguments("Canada.Ontario.HighSchool.HighSchoolTranscript.xml", "en-CA")]
-    [Arguments("Canada.Nova_Scotia.HighSchool.HighSchoolTranscript.xml", "en-CA")]
-    [Arguments("Canada.Nova_Scotia.HighSchool.HighSchoolTranscript2.xml", "en-CA")]
-    [Arguments("Canada.Nova_Scotia.HighSchool.HighSchoolTranscript3.xml", "en-CA")]
+    [Theory]
+    [InlineData("Canada.Ontario.HighSchool.HighSchoolTranscript.xml", "en-CA")]
+    [InlineData("Canada.Nova_Scotia.HighSchool.HighSchoolTranscript.xml", "en-CA")]
+    [InlineData("Canada.Nova_Scotia.HighSchool.HighSchoolTranscript2.xml", "en-CA")]
+    [InlineData("Canada.Nova_Scotia.HighSchool.HighSchoolTranscript3.xml", "en-CA")]
     public async Task Should_convert_highschool_pesc_to_pdf(string pescXml, string locale)
     {
         // Arrange
-        var apiClient = _daf.GetApiClient();
-        var notificationService = _daf.GetNotificationService();
-
         var request = new HighSchoolTranscriptToPdfRequest
         {
             Pesc = SampleHelper.ReadResourceAsString(pescXml),
             Locale = locale
         };
 
-        await notificationService.WaitForResourceAsync("api", KnownResourceStates.Running).WaitAsync(TimeSpan.FromSeconds(30));
-
         // Act
-        var response = await apiClient!.HighSchoolTranscriptToPdfAsync(request);
+        var response = await _apiClient!.HighSchoolTranscriptToPdfAsync(request);
 
         // Assert
-        await Assert.That(response).IsNotNull();
+        response.Should().NotBeNull();
         var headersLength = Convert.ToInt32(response.Headers["Content-Length"].First());
         using var memStream = new MemoryStream();
         await response.Stream.CopyToAsync(memStream);
-        await Assert.That(memStream.Length).IsEqualTo(headersLength);
+        memStream.Length.Should().Be(headersLength);
     }
 }
